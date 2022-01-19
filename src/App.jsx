ba136becbac3091bcc859a90edd46ef757a502ba
Egon <loliegon@gmail.com>
2022-01-19 18:47:16 +0100

    Added Page Not Found

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
-import { Routes, Route } from "react-router-dom";
+import { Navigate, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import MainApp from "./pages/MainPage/MainApp";
-import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
-  const params = useParams();
-  const [conversation, setConversation] = useState([]);
-
-  useEffect(() => {
-    if (params.conversationId) {
-    }
-  }, []);
-
   return (
     <div className="App">
       <Routes>
-        <Route path="./login" element={<LoginPage />} />
-        <Route path="./logged-in" element={<MainApp params={params} />} />
-        <Route path="./logged-in/:conversationId" element={<MainApp />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route index element={<Navigate replace to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/logged-in" element={<MainApp />} />
+        <Route path="/logged-in/:conversationId" element={<MainApp />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
--- src/pages/MainPage/MainApp.jsx
+++ src/pages/MainPage/MainApp.jsx
@@ -1,7 +1,16 @@
 import MainChatSection from "./MainChatSection/MainChatSection";
 import SidePanel from "./SidePanel/SidePanel";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function MainApp(props) {
+  const params = useParams();
+  const [conversation, setConversation] = useState([]);
+
+  useEffect(() => {
+    if (params.conversationId) {
+    }
+  }, []);
   return (
     <div className="main-wrapper">
       <SidePanel />
